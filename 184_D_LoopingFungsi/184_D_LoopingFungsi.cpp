d2d9bed907320903c16b9953013cb30c055bca28
athar <athar@THUGSHAKERZ>
2024-03-19 11:26:13 +0700

    int

---
+++ 184_D_LoopingFungsi/184_D_LoopingFungsi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	int arr[5];
+
+	for (i = 60; i > 10; i -= 10) {
+		cout << i << "Selamat Pagi Dunia" << endl;
+	}
+
+	cout << "Nilai i Terakhir : " << i << endl;
+
+	for (i = 0; i < 5; i++) {
+		cout << "Masukan Nilai Index ke-" << i << " :";
+		cin >> arr[i];
+	}
+
+	for (i = 0; i < 5; i++) {
+		cout << "Data Array ke - " << i << " :" << arr[i] << endl;
+	}
+
+	return 0;
+
+
+}
